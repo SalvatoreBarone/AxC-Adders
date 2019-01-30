eb723e30a1a46c62809dd842dfde6040899324f0
Andrea Aletto <andrea.aletto8@gmail.com>
2019-01-30 14:49:51 +0100

    Added comments to code

--- include/inexact_adders.h
+++ include/inexact_adders.h
@@ -1,3 +1,29 @@
+//
+// AxC Adders - A collection of inexact adders for model-driven development
+// Copyright (C) 2019 Andrea Aletto <andrea.aletto8@gmail.com>
+//
+// This file is part of AxC Adders.
+//
+// AxC Adders is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AxC Adders is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with AxC Adders.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/******************************************************************************
+ * @file   inexact_adders.h
+ * @author Andrea Aletto
+ * @date   30 gen 2019
+ * @brief  Header file for the static library InexactAdders
+ ******************************************************************************/
 
 #ifndef INEXACT_ADDERS_H
 #define INEXACT_ADDERS_H
@@ -13,4 +39,4 @@ int AXA1_adder(int nab, int a, int b);
 int AXA2_adder(int nab, int a, int b);
 int AXA3_adder(int nab, int a, int b);
 
-#endif
+#endif /* INEXACT_ADDERS_H */
--- src/InAx3_adder.cpp
+++ src/InAx3_adder.cpp
@@ -1,3 +1,30 @@
+//
+// AxC Adders - A collection of inexact adders for model-driven development
+// Copyright (C) 2019 Andrea Aletto <andrea.aletto8@gmail.com>
+//
+// This file is part of AxC Adders.
+//
+// AxC Adders is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AxC Adders is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with AxC Adders.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/******************************************************************************
+ * @file   InAx3_adder.cpp
+ * @author Andrea Aletto
+ * @date   30 gen 2019
+ * @brief  Implementation of InAx3 inexact hardware cell
+ ******************************************************************************/
+
 #include "inexact_adders.h"
 #include <stdlib.h>
 #include <math.h>
